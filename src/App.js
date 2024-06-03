c12568bb5a4a02ed26edad5861ae2503f05d4804
chrisIyer <hellohotbox420@gmail.com>
2024-06-03 16:12:09 -0400

    adding item, unenergetically.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 import {useState} from 'react';
+import './components/CreateShoppingItem'
+import CreateShoppingItem from './components/CreateShoppingItem';
 
-const shoppingCart = [
-  {
-    id: 1,
-    product: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    qty: 1,
-    price: 109.95,
-  },
-  {
-    id: 2,
-    product: 'Mens Casual Premium Slim Fit T-Shirts ',
-    qty: 1,
-    price: 22.3,
-  },
-  {
-    id: 3,
-    product: 'Mens Cotton Jacket',
-    qty: 2,
-    price: 55.99,
-  },
-  {
-    id: 4,
-    product: 'Mens Casual Slim Fit',
-    qty: 1,
-    price: 15.99,
-  },
-  {
-    id: 5,
-    product: 'Mens Casual Slim Fit',
-    qty: 4,
-    price: 22.06,
-  }
-];
+
 export default function App() {
   const [item, setItem] = useState({
     id: 0,
@@ -39,12 +10,52 @@ export default function App() {
     qty: 0,
     price: 0
   })
+  const [shoppingCart, setShoppingCart] = useState([
+    {
+      id: 1,
+      product: 'Green Jacket',
+      qty: 1,
+      price: 109.95,
+    },
+    {
+      id: 2,
+      product: 'Green Pants ',
+      qty: 1,
+      price: 22.3,
+    },
+    {
+      id: 3,
+      product: 'White Shoes',
+      qty: 2,
+      price: 55.99,
+    },
+    {
+      id: 4,
+      product: 'T-Shirt',
+      qty: 1,
+      price: 15.99,
+    },
+    {
+      id: 5,
+      product: 'White Belt',
+      qty: 4,
+      price: 22.06,
+    }, {
+    id: 6, 
+    product: 'White Bag',
+    qty: 1,
+    price: 139.99,}
+  
+  ])
 
   let total = shoppingCart.reduce((a, b) => {
     return a + (b.qty * b.price)/shoppingCart.length;
   }, 0);
   return (
     <div className="container text-center mt-3">
+   <>
+   <CreateShoppingItem/>
+   </>
       <h1>Shopping Cart</h1>
       <h5>Average: ${total.toFixed(2)}</h5>
       <button className="btn btn-primary mt-3">Checkout</button>
