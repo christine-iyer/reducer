ce66b1ae2021e88d3256be69c79e2d7a0b5a7823
chrisIyer <hellohotbox420@gmail.com>
2024-06-03 16:52:02 -0400

    changed input type

--- src/components/CreateShoppingItem.js
+++ src/components/CreateShoppingItem.js
@@ -5,9 +5,9 @@ export default function CreateShoppingItem({item, addItem, handleChange}) {
     <div>CreateShoppingItem
      <input type="text" placeholder="Create" value={item.product} />
 
-     <input type="number" placeholder="Quantity" value={item.qty} onChange={handleChange} />
+     <input className="number" type="number" placeholder="Quantity" value={item.qty} onChange={handleChange} />
      <input type="number" placeholder="Price" value={item.price} onChange={handleChange} />
-     <button type='submit' onClick={addItem} onChange={handleChange} />
+     <button className="button" type='submit' onClick={addItem} onChange={handleChange} />
     </div>
   )
 }
