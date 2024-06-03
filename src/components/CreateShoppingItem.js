e1b1f81f26e133761d0fdba9895f01ccb09a02b7
chrisIyer <hellohotbox420@gmail.com>
2024-06-03 16:50:45 -0400

    adding stuff

--- src/components/CreateShoppingItem.js
+++ src/components/CreateShoppingItem.js
@@ -5,8 +5,8 @@ export default function CreateShoppingItem({item, addItem, handleChange}) {
     <div>CreateShoppingItem
      <input type="text" placeholder="Create" value={item.product} />
 
-     <input type="text" placeholder="Quantity" value={item.qty} onChange={handleChange} />
-     <input type="text" placeholder="Price" value={item.price} onChange={handleChange} />
+     <input type="number" placeholder="Quantity" value={item.qty} onChange={handleChange} />
+     <input type="number" placeholder="Price" value={item.price} onChange={handleChange} />
      <button type='submit' onClick={addItem} onChange={handleChange} />
     </div>
   )
